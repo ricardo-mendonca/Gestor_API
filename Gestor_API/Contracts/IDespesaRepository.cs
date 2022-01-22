f676a750363b066781107ba5c233d92ee5d80afd
Ricardo Mendonca <ricardo-mendonca@live.com>
2022-01-22 19:18:54 -0300

    Atualização despesas

---
+++ Gestor_API/Contracts/IDespesaRepository.cs
@@ -0,0 +1,22 @@
+﻿using Gestor_API.Entities.Contas;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Gestor_API.Contracts
+{
+    public interface IDespesaRepository
+    {
+		public Task<IEnumerable<Despesa>> GetDespesas(int id_usuario, int cd_mes, int cd_ano );
+		public Task<Despesa> GetDespesasId(int id_usuario,int Id);
+
+		public Task<IEnumerable<Despesa>> GetUsuarioNome(int id_usuario,int cd_tipo_despesa);
+
+		public Task<Despesa> CreateDespesa(Despesa despesa);
+
+		public Task UpdateDespesa(Despesa despesa);
+
+		public Task DeleteDespesa(int id_usuario, int Id);
+		//public Task<Usuario> GetCompanyByEmployeeId(int id);
+		//public Task<Usuario> GetCompanyEmployeesMultipleResults(int id);
+	}
+}
---
+++ Gestor_API/Controllers/DespesaController.cs
@@ -0,0 +1,35 @@
+﻿using Gestor_API.Contracts;
+using Gestor_API.Entities.Contas;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Gestor_API.Controllers
+{
+    [Route("v1/Despesa")]
+    [ApiController]
+    public class DespesaController : Controller
+    {
+        private readonly IDespesaRepository _despesaRepo;
+
+        public DespesaController(IDespesaRepository despesaRepo)
+        {
+            _despesaRepo = despesaRepo;
+
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> CreateDespesa(Despesa despesa)
+        {
+
+            var usuarios = await _despesaRepo.CreateDespesa(despesa);
+            if (usuarios == null) return BadRequest(new { message = "Nehum usuário localizado." });
+
+            return Ok(usuarios);
+
+        }
+
+
+    }
+}
