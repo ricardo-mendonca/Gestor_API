613071569577d616d1892f4a77bfd1fde2cf8a35
Ricardo Mendonca <ricardo-mendonca@live.com>
2022-01-22 21:12:41 -0300

    Atualização despesas Controller

--- Gestor_API/Contracts/IDespesaRepository.cs
+++ Gestor_API/Contracts/IDespesaRepository.cs
@@ -6,15 +6,15 @@ namespace Gestor_API.Contracts
 {
     public interface IDespesaRepository
     {
-		public Task<IEnumerable<Despesa>> GetDespesas(int id_usuario, int cd_mes, int cd_ano );
-		public Task<Despesa> GetDespesasId(int id_usuario,int Id);
-
-		public Task<IEnumerable<Despesa>> GetUsuarioNome(int id_usuario,int cd_tipo_despesa);
 
 		public Task<Despesa> CreateDespesa(Despesa despesa);
+		public Task<IEnumerable<Despesa>> GetDespesas(int id_usuario, int cd_mes, int cd_ano );
 
+
+
+		public Task<Despesa> GetDespesasId(int id_usuario,int Id);
+		public Task<IEnumerable<Despesa>> GetUsuarioNome(int id_usuario,int cd_tipo_despesa);
 		public Task UpdateDespesa(Despesa despesa);
-
 		public Task DeleteDespesa(int id_usuario, int Id);
 		//public Task<Usuario> GetCompanyByEmployeeId(int id);
 		//public Task<Usuario> GetCompanyEmployeesMultipleResults(int id);
